7f2a08b79859256fd1dda1e64c7c48496de5eabe
Dragos Sutu <dragos.sutu@gmail.com>
2017-07-31 00:24:24 +0300

    Add Geth development Docker container run files.

---
+++ env/skipper/run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+currentDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+blockchainTestData="${currentDir}/../blockchainTestData/local"
+
+# Cleanup
+docker system prune --force
+
+# Initialize blockchain
+docker run \
+    --rm \
+    --tty \
+    --user $(id -u):$(id -g) \
+    --volume ${blockchainTestData}:/tmp/blockchainData \
+    drgsutu/ethereum-client-go:alpine --config config.toml init customGenesis.json
+
+# Run Geth node
+docker run \
+    --detach \
+    --name geth \
+    --user $(id -u):$(id -g) \
+    --volume ${blockchainTestData}:/tmp/blockchainData \
+    drgsutu/ethereum-client-go:alpine --config config.toml
