9f3f7f1f7bfb6b4555573e51a3bdbff094b7fe7d
Dragos Sutu <dragos.sutu@gmail.com>
2017-08-03 00:03:59 +0300

    Add etherbase address to miner.

--- env/skipper/run-miner.sh
+++ env/skipper/run-miner.sh
@@ -5,6 +5,10 @@ set -e
 currentDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 blockchainTestData="${currentDir}/../blockchainTestData/local"
 
+# get first address alphabetically
+firstAccount=$(ls ${blockchainTestData}/ethereum/keystore/ | head -1)
+etherbase="${firstAccount: -40}"
+
 # Run Geth miner
 docker container run \
     --interactive \
@@ -14,4 +18,4 @@ docker container run \
     --tty \
     --volume "${blockchainTestData}/ethash":/tmp/.ethash \
     --volume "${blockchainTestData}/ethereum":/tmp/.ethereum \
-    drgsutu/ethereum-client-go:alpine --mine --minerthreads=4
+    drgsutu/ethereum-client-go:alpine --etherbase ${etherbase} --mine --minerthreads=4
