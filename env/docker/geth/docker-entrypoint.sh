6a70432038b5b6df0a7ff4143c2006c2cf6af415
Dragos Sutu <dragos.sutu@gmail.com>
2017-08-15 16:52:50 +0300

    Initialize blockchain before adding accounts, without altering genesis block json, so other nodes can be joined using the same genesis block.

--- env/docker/geth/docker-entrypoint.sh
+++ env/docker/geth/docker-entrypoint.sh
@@ -2,13 +2,18 @@
 
 set -uo pipefail
 
-numberOfAccounts=${NUMBER_OF_ACCOUNTS:-4}
+numberOfAccounts=${NUMBER_OF_ACCOUNTS:-2}
 keystoreDir='/tmp/.ethereum/keystore'
 outputFormat="\n**** %s\n"
 
 # if command starts with an option, prepend executable /geth
 if [ "${1}" != "/geth" ]; then
 	set -- /geth --config config.toml "$@"
+fi
+
+if [ ! -d '/tmp/.ethereum/geth/chaindata' ]; then
+    printf "$outputFormat" "Initializing genesis block"
+    /geth --config config.toml init /customGenesis.json
 fi
 
 # make sure keystore dir exists
@@ -22,19 +27,6 @@ if [ ! "$(ls -A ${keystoreDir})" ]; then
         /geth --config config.toml account new --password password.txt
         let "numberOfAccounts=numberOfAccounts-1"
     done
-
-    # temporary hack to fix user privileges problems
-    cp /customGenesisExample.json /tmp/customGenesis.json
-
-    printf "$outputFormat" "Adding accounts to genesis block json"
-    for f in $(ls ${keystoreDir}); do
-        address=$(jq -r '.address' "$keystoreDir/$f")
-        jq --arg address $address '.alloc += {($address):{"balance":"20000000000000000000"}}' /tmp/customGenesis.json > /tmp/tmpCustomGenesis.json
-        mv /tmp/tmpCustomGenesis.json /tmp/customGenesis.json
-    done
-
-    printf "$outputFormat" "**** Initializing genesis block"
-    /geth --config config.toml init /tmp/customGenesis.json
 fi
 
 printf "$outputFormat" "Running container CMD"
