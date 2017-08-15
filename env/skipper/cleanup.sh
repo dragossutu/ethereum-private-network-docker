8d273eb7bcd0f11d8ccd8c129e2da8be14e55ca5
Dragos Sutu <dragos.sutu@gmail.com>
2017-08-16 00:39:10 +0300

    Add network removal at cleanup, fix label typo.

--- env/skipper/cleanup.sh
+++ env/skipper/cleanup.sh
@@ -2,5 +2,9 @@
 
 docker container ps \
     --all \
-    --filter label=project=eth-dev \
+    --filter label=project=eth-dapp-dev \
     --quiet | xargs --no-run-if-empty docker container rm --force --volumes
+
+docker network ls \
+    --filter label=project=eth-dapp-dev \
+    --quiet | xargs --no-run-if-empty docker network rm
