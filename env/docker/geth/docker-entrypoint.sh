2a837cd2ec95bcbfdabd422c22c8411e0c87c190
dragos sutu <dragos.sutu@gmail.com>
2017-12-29 13:25:54 +0700

    Add some comments, change quotes.

--- env/docker/geth/docker-entrypoint.sh
+++ env/docker/geth/docker-entrypoint.sh
@@ -3,24 +3,26 @@
 set -uo pipefail
 
 numberOfAccounts=${NUMBER_OF_ACCOUNTS:-2}
-keystoreDir='/tmp/.ethereum/keystore'
+keystoreDir="/tmp/.ethereum/keystore"
 outputFormat="\n**** %s\n"
 
-# if command starts with an option, prepend executable /geth
+# if command starts with an option, prepend executable /geth with the config
 if [ "${1}" != "/geth" ]; then
 	set -- /geth --config config.toml "$@"
 fi
 
-if [ ! -d '/tmp/.ethereum/geth/chaindata' ]; then
+# initialize genesis block if it wasn't already initialized
+if [ ! -d "/tmp/.ethereum/geth/chaindata" ]; then
     printf "$outputFormat" "Initializing genesis block"
     /geth --config config.toml init /customGenesis.json
 fi
 
 # make sure keystore dir exists
 if [ -d "$keystoreDir" ]; then
-    mkdir -p ${keystoreDir}
+    mkdir --parents ${keystoreDir}
 fi
 
+# create accounts if no accounts exist
 if [ ! "$(ls -A ${keystoreDir})" ]; then
     printf "$outputFormat" "Creating accounts"
     while [ "$numberOfAccounts" -gt 0 ]; do
