5da7ad4fa44f8fb9ef4afa3dd62a26fb16565d10
Dragos Sutu <dragos.sutu@gmail.com>
2018-01-31 08:12:30 +0200

    Add geth flags to disable IPC, add node identity and unlock accounts.

--- env/skipper/run-miner.sh
+++ env/skipper/run-miner.sh
@@ -5,17 +5,26 @@ set -e
 currentDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 blockchainDataDir="${currentDir}/../blockchainData/local"
 
+minerIPAddress=${1:-172.20.0.2}
+
 # make sure blockchain data dir exists, else Docker will create it with root as owner
 mkdir --parents --verbose ${blockchainDataDir}/{ethash,ethereum}
 
 # Run Geth node
 docker container run \
     --detach \
-    --ip 172.20.0.2 \
+    --ip ${minerIPAddress} \
     --label project=eth-private-network \
     --name geth-miner \
     --network eth-private-network \
     --user $(id --user):$(id --group) \
     --volume "${blockchainDataDir}/ethash":/tmp/.ethash \
     --volume "${blockchainDataDir}/ethereum":/tmp/.ethereum \
-    drgsutu/ethereum-client-go:alpine --mine --minerthreads 1 --rpc --rpcaddr "0.0.0.0"
+    drgsutu/ethereum-client-go:alpine \
+        --identity "dev-miner" \
+        --ipcdisable \
+        --mine \
+        --minerthreads 1 \
+        --password /password.txt \
+        --rpc \
+        --unlock 0,1,2
